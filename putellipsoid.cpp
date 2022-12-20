16f6baa7c7fa975437c443996fea13ce64e36576
luizedu17 <113469570+luizedu17@users.noreply.github.com>
2022-12-20 20:55:33 -0300

    Update putellipsoid.cpp

--- putellipsoid.cpp
+++ putellipsoid.cpp
@@ -1,24 +1,23 @@
 #include "putellipsoid.h"
-
-
-PutEllipsoid::PutEllipsoid(int _xcenter, int _ycenter, int _zcenter, int _rx, int _ry, int _rz, float _r, float _g, float _b, float _a)
+PutEllipsoid::PutEllipsoid(int x0, int y0, int z0,int rdX,int rdY,int rdZ, float r, float g, float b, float a)
+    : p_x0(x0), p_y0(y0), p_z0(z0), p_rdX(rdX), p_rdY(rdY), p_rdZ(rdZ), p_r(r), p_g(g), p_b(b), p_a(a)
 {
-    xcenter = _xcenter;ycenter = _ycenter;zcenter = _zcenter;
-    rx = _rx;ry = _ry;rz = _rz;
-    r = _r;g = _g; b = _b;a = _a;
 }
-PutEllipsoid::~PutEllipsoid()
+void PutEllipsoid::draw(Sculptor& t)
 {
+    int i, j, k;
+    double DisX, DisY, DisZ;
 
-}
+    t.setColor(p_r, p_g, p_b, p_a);
 
-
-void PutEllipsoid::draw(Sculptor &t){
-    for (int i = xcenter-rx; i < xcenter+rx; i++) {
-        for (int j = ycenter-ry; j < ycenter+ry; j++){
-            for (int k = zcenter-rz; k < zcenter+ rz; k++){
-                if(((float)pow((i-xcenter),2)/(pow(rx,2)))+((float)pow((j-ycenter),2)/(pow(ry,2)))+((float)pow((k-zcenter),2)/(pow(rz,2)))<1.0){
-                    t.putVoxel(i,j,k);
+    for (i = p_z0 - p_rdZ; i < p_z0 + p_rdZ; i++) {
+        DisX = ((double)(i - p_z0)) / (double)(p_rdZ);
+        for (j = p_y0 - p_rdY; j < p_y0 + p_rdY; j++) {
+            DisY = ((double)(j - p_y0)) / (double)(p_rdY);
+            for (k = p_x0 - p_rdX; k < p_x0 + p_rdX; k++) {
+                DisZ = ((double)(k - p_x0)) / (double)(p_rdX);
+                if ((DisX * DisX + DisY * DisY + DisZ * DisZ) <= 1.00) {
+                    t.putVoxel(i, j, k);
                 }
             }
         }
