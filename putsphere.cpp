fb6e58462a146d426869c8c005fc6a2a249956e2
luizedu17 <113469570+luizedu17@users.noreply.github.com>
2022-12-20 20:56:24 -0300

    Update putsphere.cpp

--- putsphere.cpp
+++ putsphere.cpp
@@ -1,22 +1,23 @@
 #include "putsphere.h"
-
-
-PutSphere::PutSphere(int _xcenter, int _ycenter, int _zcenter, int _radius, float _r, float _g, float _b, float _a)
+PutSphere::PutSphere(int x0, int y0, int z0, int rd, float r, float g, float b, float a)
+    : p_x0(x0), p_y0(y0), p_z0(z0), p_rd(rd), p_r(r), p_g(g), p_b(b), p_a(a)
 {
-    xcenter = _xcenter;ycenter = _ycenter;zcenter = _zcenter;radius = _radius;
-    r = _r;g = _g;b = _b;a = _a;
 }
-PutSphere::~PutSphere()
+void PutSphere::draw(Sculptor& t)
 {
+    int i, j, k;
+    float DisX, DisY, DisZ;
 
-}
+    t.setColor(p_r, p_b, p_b, p_a);
 
-void PutSphere::draw(Sculptor &t){
-    for (int i = xcenter-radius; i < xcenter+radius; i++) {
-        for (int j = ycenter-radius; j < ycenter+radius; j++){
-            for (int k = zcenter-radius; k < zcenter+radius; k++){
-                if(((float)pow((i-xcenter),2))+((float)pow((j-ycenter),2))+((float)pow((k-zcenter),2))<=(pow(radius,2))){
-                    t.putVoxel(i,j,k);
+    for (i = p_z0 - p_rd; i < p_z0 + p_rd; i++) {
+        DisZ = ((float)(i - p_z0)) / ((float)(p_rd));
+        for (j = p_y0 - p_rd; j < p_y0 + p_rd; j++) {
+            DisY = ((float)(j - p_y0)) / ((float)(p_rd));
+            for (k = p_x0 - p_rd; k < p_x0 + p_rd; k++) {
+                DisX = ((float)(k - p_x0)) / ((float)(p_rd));
+                if ((DisX * DisX + DisY * DisY + DisZ * DisZ) <= 1.00) {
+                    t.putVoxel(i, j, k);
                 }
             }
         }
