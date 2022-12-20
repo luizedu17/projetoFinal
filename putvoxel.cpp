fb73aa2e979c65440f0c0676257de9a01e8c6aba
luizedu17 <113469570+luizedu17@users.noreply.github.com>
2022-12-20 20:56:59 -0300

    Update putvoxel.cpp

--- putvoxel.cpp
+++ putvoxel.cpp
@@ -1,18 +1,13 @@
 #include "putvoxel.h"
-
-PutVoxel::PutVoxel(int _x, int _y, int _z, float _r, float _g, float _b, float _a)
+PutVoxel::PutVoxel(int x, int y, int z, float r, float g, float b, float a)
+    : p_x(x), p_y(y), p_z(z), p_r(r), p_g(g), p_b(b), p_a(a)
 {
-    x = _x;y = _y;z = _z;
-    r = _r;g = _g;b = _b;a = _a;
+}
+void PutVoxel::draw(Sculptor& t) {
+    t.setColor(p_r, p_g, p_b, p_a);
+    t.putVoxel(p_z, p_y, p_x);
 }
 
 PutVoxel::~PutVoxel()
 {
-
-}
-
-void PutVoxel::draw(Sculptor &t)
-{
-    t.setColor(r, g, b, a);
-    t.putVoxel(x, y, z);
 }
