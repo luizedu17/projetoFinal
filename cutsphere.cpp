c411d317fd6de94be795f89b3ae265777d32d22b
luizedu17 <113469570+luizedu17@users.noreply.github.com>
2022-12-20 20:52:40 -0300

    Update cutsphere.cpp

--- cutsphere.cpp
+++ cutsphere.cpp
@@ -1,24 +1,20 @@
 #include "cutsphere.h"
-
-CutSphere::CutSphere(int _xcenter, int _ycenter, int _zcenter, int _radius )
+CutSphere::CutSphere(int x0, int y0, int z0, int rd): p_x0(x0), p_y0(y0), p_z0(z0), p_rd(rd)
 {
-    xcenter = _xcenter;
-    ycenter = _ycenter;
-    zcenter = _zcenter;
-    radius = _radius;
 }
-
-CutSphere::~CutSphere()
+void CutSphere::draw(Sculptor& t)
 {
+    int i, j, k;
+    float DisX, DisY, DisZ;
 
-}
-
-void CutSphere::draw(Sculptor &t){
-    for (int i = xcenter-radius; i < xcenter+radius; i++) {
-        for (int j = ycenter-radius; j < ycenter+radius; j++){
-            for (int k = zcenter-radius; k < zcenter+radius; k++){
-                if(((float)pow((i-xcenter),2))+((float)pow((j-ycenter),2))+((float)pow((k-zcenter),2))<=(pow(radius,2))){
-                    t.cutVoxel(i,j,k);
+    for (i = p_z0-p_rd; i < p_z0+p_rd; i++) {
+        DisZ = ((float)(i - p_z0)) / ((float)(p_rd));
+        for (j = p_y0 - p_rd; j < p_y0 + p_rd; j++) {
+            DisY = ((float)(j - p_y0)) / ((float)(p_rd));
+            for (k = p_x0 - p_rd; k< p_x0 + p_rd; k++) {
+                DisX = ((float)(k - p_x0)) / ((float)(p_rd));
+                if ((DisX * DisX + DisY * DisY + DisZ * DisZ) <= 1.00) {
+                    t.cutVoxel(i, j, k);
                 }
             }
         }
