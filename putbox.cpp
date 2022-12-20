f9ded292e5ca049b1705dc9b8ce328ee377f7fc1
luizedu17 <113469570+luizedu17@users.noreply.github.com>
2022-12-20 20:54:54 -0300

    Update putbox.cpp

--- putbox.cpp
+++ putbox.cpp
@@ -1,23 +1,17 @@
 #include "putbox.h"
-
-
-PutBox::PutBox(int _x0, int _x1, int _y0, int _y1, int _z0, int _z1, float _r, float _g, float _b, float _a)
+PutBox::PutBox(int x0, int y0, int z0, int x1, int y1, int z1, float r, float g, float b, float a)
+    : p_x0(x0), p_y0(y0), p_z0(z0), p_x1(x1), p_y1(y1), p_z1(z1), p_r(r), p_g(g), p_b(b), p_a(a)
 {
-    x0 = _x0;x1 = _x1;y0 = _y0;y1 = _y1;z0 = _z0;z1 = _z1;
-    r = _r;g = _g;b = _b;a = _a;
 }
 
-PutBox::~PutBox()
+void PutBox::draw(Sculptor& t)
 {
-
-}
-
-
-void PutBox::draw(Sculptor &t){
-    for (int i = x0; i<=x1; i++){
-        for (int j = y0; j<=y1; j++){
-            for (int k = z0; k<=z1; k++){
-                t.putVoxel(i,j,k);
+    t.setColor(p_r, p_g, p_b, p_a);
+    int i, j, k;
+    for (i = p_z0; i < p_z1; i++) {
+        for (j = p_y0; j < p_y1; j++) {
+            for (k = p_x0; k < p_x1; k++) {
+                t.putVoxel(i, j, k);
             }
         }
     }
