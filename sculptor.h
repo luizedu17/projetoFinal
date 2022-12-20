5e574d637b7cba5d7773aa4e03f2fa8f4f71fb09
luizedu17 <113469570+luizedu17@users.noreply.github.com>
2022-12-20 20:57:55 -0300

    Update sculptor.h

--- sculptor.h
+++ sculptor.h
@@ -1,39 +1,30 @@
 #ifndef SCULPTOR_H
 #define SCULPTOR_H
-
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <vector>
-#include <cmath>
-
-using namespace std;
-
-
+#include<string>
 struct Voxel {
-  float r,g,b;
-  float a;
-  bool isOn;
+    float r, g, b;	
+    float a;		
+    bool isOn;		
 };
 
 class Sculptor {
 protected:
-  int nx, ny, nz;
-  Voxel ***v;
-  float r,g,b,a;
+    Voxel ***v;			
+    int nx, ny, nz;		
+    float r, g, b, a;	
 public:
-  Sculptor();
-  Sculptor(int _nx, int _ny, int _nz);
-  ~Sculptor();
-  void setColor(float red, float green, float blue, float alpha);
-  void putVoxel(int x, int y, int z);
-  void cutVoxel(int x, int y, int z);
-  int getNx();
-  int getNy();
-  int getNz();
-  vector<vector<Voxel>> planeXY(float zFactor);
-  vector<vector<vector<Voxel>>> plane3D();
-  void writeOFF(string filename);
+    Sculptor(int _nx, int _ny, int _nz);
+    ~Sculptor();
+    void setColor(float r, float g, float b, float alpha);
+    void putVoxel(int z, int y, int x);
+    void cutVoxel(int z, int y, int x);
+    void putBox(int x0, int x1, int y0, int y1, int z0, int z1);
+    void cutBox(int x0, int x1, int y0, int y1, int z0, int z1);
+    void putSphere(int xcenter, int ycenter, int zcenter, int radius);
+    void cutSphere(int xcenter, int ycenter, int zcenter, int radius);
+    void putEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
+    void cutEllipsoid(int xcenter, int ycenter, int zcenter, int rx, int ry, int rz);
+    void writeOFF(char* filename);
 };
 
 #endif // SCULPTOR_H
